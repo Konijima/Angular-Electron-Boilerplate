6659bc79077245bb3b6aa0c22e63b53db46b84d1
Konijima <mathieugermain1991@gmail.com>
2023-04-19 00:19:09 -0400

    Update apiDeclaration.js to include exported interfaces and prettier formatting
    In this commit, I have made changes to the apiDeclaration.js file to ensure that it now properly allows for the exporting of interfaces. This was a necessary update to ensure that our application's API functions as intended.
    
    Additionally, I have added prettier formatting to the code, which helps to improve the overall readability and consistency of the file. This will make it easier for other developers to work with this code in the future.
    
    Overall, this update should help to improve the functionality and maintainability of our application's API.

--- electron/API/test.ts
+++ electron/API/test.ts
@@ -1,6 +1,17 @@
-// Create the object that you want to use in the renderer process
-// This object must be exposed in the preload script (preload.ts)
-export const Test = {
+
+/**
+ * This is the interface that you want to use in the renderer process
+ */
+export interface TestInterface {
+    hello(): string;
+    world(): string;
+}
+
+/**
+ * Create the object that you want to use in the renderer process
+ * This object must be exposed in the preload script (preload.ts)
+ */
+export const Test: TestInterface = {
     hello(): string {
         return 'Hello';
     },
